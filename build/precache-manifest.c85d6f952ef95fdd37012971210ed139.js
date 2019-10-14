33af01c94be5191c9c5d08353a9349d5c2b921e7
lijiyeo <ljy981017>
2019-10-14 15:44:56 +0800

    one

---
+++ build/precache-manifest.c85d6f952ef95fdd37012971210ed139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0649dd2ef76a8e43ad8f8799ac51d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ae07069383ab96f01a",
+    "url": "/static/css/main.b5ad2cce.chunk.css"
+  },
+  {
+    "revision": "2d6c7706213d80ec8f73",
+    "url": "/static/js/2.f8aebe7d.chunk.js"
+  },
+  {
+    "revision": "88ae07069383ab96f01a",
+    "url": "/static/js/main.f2ce3411.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
